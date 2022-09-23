64cf01941cba9a99ff5e3f32fa91392f43315b9a
pbsuazocod <pbsuazocod@gmail.com>
2022-09-23 04:18:54 -0800

    implemented formik

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,94 +1,63 @@
-import { useFormik } from "formik";
+import { useFormik, Formik } from "formik";
 import React from "react";
-
-const validate = (values) => {
-  const errors = {};
-  if (!values.firstName) {
-    errors.firstName = "Required";
-  } else if (values.firstName.length > 15) {
-    errors.firstName = "Must be 15 characters or less";
-  }
-
-  if (!values.lastName) {
-    errors.lastName = "Required";
-  } else if (values.lastName.length > 20) {
-    errors.lastName = "Must be 20 characters or less";
-  }
-
-  if (!values.email) {
-    errors.email = "Required";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid email address";
-  }
-
-  return errors;
-};
+import * as Yup from "yup";
 
 function Form() {
-  const formik = useFormik({
-    initialValues: {
-      firstName: "",
-      lastName: "",
-      email: "",
-    },
-    validate,
-
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
+  
 
   return (
-    <div className=" ">
-      <form className="flex flex-col space-y-4" onSubmit={formik.handleSubmit}>
-        <label className="bg-gray-300" htmlFor="firstName">
-          First name
-        </label>
+    
+    <Formik
+    initialValues={{ firstName: '', lastName: '', email: '' }}
+    validationSchema={Yup.object({
+      firstName: Yup.string()
+        .max(15, 'Must be 15 characters or less')
+        .required('Required'),
+      lastName: Yup.string()
+        .max(20, 'Must be 20 characters or less')
+        .required('Required'),
+      email: Yup.string().email('Invalid email address').required('Required'),
+    })}
+    onSubmit={(values, { setSubmitting }) => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2));
+        setSubmitting(false);
+      }, 400);
+    }}
+  >
+    {formik => (
+      <form className=" flex flex-col space-y-4" onSubmit={formik.handleSubmit}>
+        <label htmlFor="firstName">First Name</label>
         <input
           id="firstName"
-          name="firstName"
-          type="firstName"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.firstName}
+          type="text"
+          {...formik.getFieldProps('firstName')}
         />
         {formik.touched.firstName && formik.errors.firstName ? (
-         <div className="text-red-300">{formik.errors.firstName}</div>
-       ) : null}
-        <label className="bg-gray-300" htmlFor="lastName">
-          Last Name
-        </label>
+          <div>{formik.errors.firstName}</div>
+        ) : null}
+
+        <label htmlFor="lastName">Last Name</label>
         <input
           id="lastName"
-          name="lastName"
-          type="lastName"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.lastName}
+          type="text"
+          {...formik.getFieldProps('lastName')}
         />
         {formik.touched.lastName && formik.errors.lastName ? (
-         <div className="text-red-300">{formik.errors.lastName}</div>
-       ) : null}
-        <label className="bg-gray-300" htmlFor="email">
-          Email Address
-        </label>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.email}
-        />
-         {formik.touched.email && formik.errors.email ? (
-         <div className="text-red-300">{formik.errors.email}</div>
-       ) : null}
-        <button className="bg-red-300" type="submit">
-          Submit
-        </button>
+          <div>{formik.errors.lastName}</div>
+        ) : null}
+
+        <label htmlFor="email">Email Address</label>
+        <input id="email" type="email" {...formik.getFieldProps('email')} />
+        {formik.touched.email && formik.errors.email ? (
+          <div>{formik.errors.email}</div>
+        ) : null}
+
+        <button className="bg-red-300" type="submit">Submit</button>
       </form>
-    </div>
-  );
+    )}
+  </Formik>
+  )
 }
 
 export default Form;
